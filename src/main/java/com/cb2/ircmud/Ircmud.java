6caffe5acc3a2434cec692b98d4074cd980ba877
Olavi Lintumäki <atomimalli@gmail.com>
2013-11-12 02:03:23 +0200

    Spring and roo enabled

--- src/main/java/com/cb2/ircmud/Ircmud.java
+++ src/main/java/com/cb2/ircmud/Ircmud.java
@@ -3,14 +3,35 @@ package com.cb2.ircmud;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
+
+import com.cb2.ircmud.domain.Room;
+
+@Component
 public class Ircmud {
 	
 	public static String globalServerName = new String("IrcMud");
 	public static int    globalServerPort = 6667;
 
-	public static void main(String[] args) {
-
+	public void main(String[] args) {
+		Room room = new Room();		//using plain roo generated entity
+		room.setName("olohuone");
+		room.persist();	
+		room = new Room();
+		room.setName("niitty");
+		room.persist();	 						//save to database
+		
+		List<Room> rooms = Room.findAllRooms(); //fetch from database
+		
+		System.out.println(rooms);
+		
 		switch(args.length) {
 			case 0: break;
 			case 1: 
@@ -24,6 +45,7 @@ public class Ircmud {
 		}
 
 		try {
+			System.out.println("muodostetaan yhteys");
 			ServerSocket serverSocket = new ServerSocket(globalServerPort);
 
 			while (true) {
@@ -34,7 +56,7 @@ public class Ircmud {
 			}
 
 		} catch(IOException e) {
-			
+			System.out.println(e);
 		}
 		
 		
