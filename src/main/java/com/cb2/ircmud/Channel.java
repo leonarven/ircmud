e4650f969d1ab7ae1d944a816df4929283bdae11
Miro Nieminen <leonarven@gmail.com>
2013-11-15 16:35:04 +0200

    More edit to IrcReply

--- src/main/java/com/cb2/ircmud/Channel.java
+++ src/main/java/com/cb2/ircmud/Channel.java
@@ -45,32 +45,32 @@ public class Channel {
 			for(IrcUser _user : channelMembers) {
 				// TODO H/G => here/gone
 				// TODO: H/G:n perään tulee henkilön statuksen indikoiva merkki (+/@)
-				IrcReply whoReply = new IrcReply(IrcServer.globalServerName, "352", user.getNickname()+" "+this.name+" "+_user.getNickname()+" "+_user.getHostname()+" "+IrcServer.globalServerName+" "+_user.getNickname()+" H :0 0 " + _user.getRealname());
+				IrcReply whoReply = IrcReply.serverReply(Const.RPL_WHOREPLY, user.getNickname(), this.name, _user.getNickname(), _user.getHostname(), IrcServer.globalServerName, _user.getNickname(), "H", "0 0 " + _user.getRealname());
 				user.sendReply(whoReply);
 			}
-			IrcReply whoEndReply = new IrcReply(IrcServer.globalServerName, "315", user.getNickname()+" " + this.name + " :End of /WHO list.");
+			IrcReply whoEndReply = IrcReply.serverReply(Const.RPL_ENDOFWHO, user.getNickname(), this.name, "End of /WHO list.");
 			user.sendReply(whoEndReply);
 	}
 	
 	public void addMember(IrcUser user) {
 		synchronized (channelMembers) {
-			IrcReply joinReply = new IrcReply(user, "JOIN", this.name);
+			IrcReply joinReply = new IrcReply(user, "JOIN", this.name, "");
 			user.sendReply(joinReply);
 			sendReplyToAll(joinReply);
 			
 			if (this.topic != null) {
-				IrcReply topicReply = new IrcReply(IrcServer.globalServerName, "332", user.nickname+" "+this.name+" :"+this.topic);
+				IrcReply topicReply = IrcReply.serverReply(Const.RPL_TOPIC, user.nickname, this.name, this.topic);
 				user.sendReply(topicReply);
 				
-				IrcReply topicDateReply = new IrcReply(IrcServer.globalServerName, "333", user.nickname+" "+this.name+" admin!admin@IrcMud 0");
+				IrcReply topicDateReply = IrcReply.serverReply("333", user.nickname, this.name, "admin!admin@IrcMud", "0", "");
 				user.sendReply(topicDateReply);
 			}
 			String userlist = user.nickname;
 			for(IrcUser _user : channelMembers) userlist = userlist + " " +  _user.nickname;
-			IrcReply namesReply = new IrcReply(IrcServer.globalServerName, "353", user.nickname+" @ "+ this.name + " :" + userlist.trim());
+			IrcReply namesReply = new IrcReply(IrcServer.globalServerName, Const.RPL_NAMREPLY, user.nickname, "@", this.name, userlist.trim());
 			user.sendReply(namesReply);
 			
-			IrcReply namesEndReply = new IrcReply(IrcServer.globalServerName, "366", user.nickname+" " + this.name + " :End of /NAMES list.");
+			IrcReply namesEndReply = new IrcReply(IrcServer.globalServerName, "366", user.nickname, this.name , "End of /NAMES list.");
 			user.sendReply(namesEndReply);
 			
 			channelMembers.add(user);
@@ -81,14 +81,14 @@ public class Channel {
 		synchronized (channelMembers) {
 			channelMembers.remove(user);
 		}
-		sendReplyToAll(new IrcReply(user, "PART", ":" + msg));
+		sendReplyToAll(new IrcReply(user, "PART", msg));
 	}
 	
 	public void memberQuit(IrcUser user, String msg) {
 		synchronized (channelMembers) {
 			channelMembers.remove(user);
 		}
-		sendReplyToAll(new IrcReply(user, "QUIT", ":" + msg));
+		sendReplyToAll(new IrcReply(user, "QUIT", msg));
 	}
 	
 	
