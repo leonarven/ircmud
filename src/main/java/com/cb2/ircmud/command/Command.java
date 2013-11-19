69b17c0568dca06e888cd38dd71d67817f59da3d
Lassi Hämäläinen <latexi95@gmail.com>
2013-11-19 21:06:24 +0200

    More command parsing.

--- src/main/java/com/cb2/ircmud/command/Command.java
+++ src/main/java/com/cb2/ircmud/command/Command.java
@@ -1,15 +1,20 @@
 package com.cb2.ircmud.command;
 
 
+import java.util.Vector;
+
 import com.cb2.ircmud.command.CommandDefinition;
 
 public class Command {
 	private CommandDefinition definition;
-	
+	private Vector<CommandParameter> parameters;
 	Command(CommandDefinition def) {
 		definition = def;
 	}
 	
 	CommandDefinition getCommandDefinition() { return definition; }
+	
+	void addParameter(CommandParameter param) { parameters.add(param); }
+	Vector<CommandParameter> getParameters() { return parameters; }
 	
 }
--- src/main/java/com/cb2/ircmud/command/CommandParser.java
+++ src/main/java/com/cb2/ircmud/command/CommandParser.java
@@ -1,5 +1,6 @@
 package com.cb2.ircmud.command;
 
+import com.cb2.ircmud.Console;
 import com.cb2.ircmud.command.Command;
 import com.cb2.ircmud.command.CommandDefinition;
 
@@ -13,9 +14,17 @@ import java.util.regex.Pattern;
 public class CommandParser {
 	private Vector<CommandDefinition> commandDefinitions;
 	private Pattern defaultPreParamPattern;
+	private Pattern integerPattern;
+	private Pattern locationPattern;
+	private Pattern itemPattern;
+	private Pattern stringPattern;
 	
 	public CommandParser() {
-		defaultPreParamPattern = Pattern.compile("^\\s");
+		defaultPreParamPattern = Pattern.compile("^\\s+");
+		integerPattern = Pattern.compile("^-?[1-9][0-9]*");
+		stringPattern = Pattern.compile("^\"(.*)\"");
+		locationPattern = Pattern.compile("^(the\\s+)?([a-zA-Z]+)");
+		itemPattern = Pattern.compile("^(a|the|all|every|my|[0-9]+)?\\s+ ([a-z]+)", Pattern.CASE_INSENSITIVE);
 	}
 	
 	void addCommandDefinition(CommandDefinition def) {
@@ -62,7 +71,7 @@ public class CommandParser {
 			
 			Matcher matcher = preParamPattern.matcher(cmdLine);
 			if (matcher.lookingAt()) {
-				cmdLine = cmdLine.substring(0, matcher.end()); //remove PreParamPattern
+				cmdLine = cmdLine.substring(matcher.end()); //remove PreParamPattern
 			} else {
 				throw new CommandParsingException(MessageFormat.format("Pre param pattern \"{0}\" didn't match the start of \"{1}\"", preParamPattern.toString(), cmdLine));
 			}
@@ -92,25 +101,97 @@ public class CommandParser {
 	}
 	
 	public String parseInteger(String cmdLine, Command cmd)  throws CommandParsingException {
-		
-		
-		return null;
+		Matcher matcher = integerPattern.matcher(cmdLine);
+		if (matcher.lookingAt()) {
+			String intString = cmdLine.substring(0, matcher.end());
+			cmdLine = cmdLine.substring(matcher.end()); //remove
+			
+			try {
+				int val = Integer.parseInt(intString);
+				cmd.addParameter(new IntegerParameter(val));
+				return cmdLine;
+			} catch (NumberFormatException e) {
+				throw new CommandParsingException(MessageFormat.format("Integer parsing failed \"{0}\"", intString));
+			}
+			
+			
+		} else {
+			throw new CommandParsingException(MessageFormat.format("Expecting integer... \"{0}\"", cmdLine));
+		}
 	}
 	public String parseString(String cmdLine, Command cmd)  throws CommandParsingException {
-		
-		
+		Matcher matcher = locationPattern.matcher(cmdLine);
+		if (matcher.lookingAt()) {
+			String stringVal = matcher.group(1);
+			cmdLine = cmdLine.substring(matcher.end()); //remove
+			cmd.addParameter(new StringParameter(stringVal));
+			
+		} else {
+			throw new CommandParsingException(MessageFormat.format("Expecting location... \"{0}\"", cmdLine));
+		}
 		return null;
 	}
 	
 	public String parseLocation(String cmdLine, Command cmd)  throws CommandParsingException {
-		
-		
+		Matcher matcher = locationPattern.matcher(cmdLine);
+		if (matcher.lookingAt()) {
+			String locationString = matcher.group(2);
+			cmdLine = cmdLine.substring(matcher.end()); //remove
+			cmd.addParameter(new LocationParameter(locationString));
+			
+		} else {
+			throw new CommandParsingException(MessageFormat.format("Expecting location... \"{0}\"", cmdLine));
+		}
 		return null;
 	}
 	public String parseItem(String cmdLine, Command cmd)  throws CommandParsingException {
-		
-		
-		return null;
+		Matcher matcher = itemPattern.matcher(cmdLine);
+		if (matcher.lookingAt()) {
+			String preString = matcher.group(1).toLowerCase();
+			String itemName = matcher.group(2);
+			cmdLine = cmdLine.substring(matcher.end()); //remove
+			
+			ItemParameter param = null;
+			if (preString.isEmpty()) {
+				param = new ItemParameter(1, itemName);
+			} else if (preString.equals("a")) {
+				param = new ItemParameter(1, itemName);
+			} else if (preString.equals("the")) {
+				param = new ItemParameter(ItemParameter.QuantityType.Specific, itemName);
+			} else if (preString.equals("my")) {
+				param = new ItemParameter(ItemParameter.QuantityType.Specific, itemName);
+				param.setOwner(ItemParameter.Owner.Caller);
+			} else if (preString.equals("all") || preString.equals("every")) {
+				param = new ItemParameter(ItemParameter.QuantityType.All, itemName);
+			} else if (Character.isDigit(preString.charAt(0))) {
+				try {
+					int val = Integer.parseInt(preString);
+					param = new ItemParameter(val, itemName);
+				} catch (NumberFormatException e) {
+					throw new CommandParsingException(MessageFormat.format("Integer parsing failed \"{0}\"", preString));
+				}
+			} else {
+				throw new CommandParsingException("WTF error");
+			}
+			cmd.addParameter(param);
+			return cmdLine;
+			
+		} else {
+			throw new CommandParsingException(MessageFormat.format("Expecting location... \"{0}\"", cmdLine));
+		}
 	}
+	/*
+	public static void main(String[] args) {
+		CommandParser parser = new CommandParser();
+		CommandDefinition runDefinition = new CommandDefinition("run", "runs?");
+		runDefinition.addParameter(Pattern.compile("^\\s+(to\\s+)?", Pattern.CASE_INSENSITIVE), CommandParameter.Type.Location);
+		parser.addCommandDefinition(runDefinition);
+		
+		try {
+			Command cmd = parser.parse("run to the west");
+		} catch (CommandParsingException ex) {
+			Console.out(ex);
+		}
+	}*/
 	
 }
