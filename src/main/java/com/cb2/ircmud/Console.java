93c154265d87453bfe18a5125a33f43dcba813f4
Miro Nieminen <leonarven@gmail.com>
2013-11-19 18:59:55 +0200

    Added Console.err

--- src/main/java/com/cb2/ircmud/Console.java
+++ src/main/java/com/cb2/ircmud/Console.java
@@ -5,32 +5,53 @@ import java.util.ArrayList;
 
 public class Console {
 	public static long _startTime = new Date().getTime(); 
-	public static void init() {
-		
-	}
+
+	private static String[] ObjArrToStrArr(Object[] arr) {
+		String[] output = new String[arr.length];
+		for( int i = 0; i < arr.length; i++)  output[i] = arr[i].toString();
+		return output;
+	}	
 	
+	/*
+	 */
 	public static String getTimestamp() {
 		String time = new String();
 		time += (new Date().getTime()) - Console._startTime;
 		return time;
 	}
-	
+
+	/*
+	 */
 	public static void out(String... args) {
 		String string = getTimestamp();
 		for( String str : args ) string += ":\t"+str;
 		System.out.println(string);
 	}
 	public static void out(Object... args) {
-		String[] output = new String[args.length];
-		for( int i = 0; i < args.length; i++)  output[i] = args[i].toString();
-		out(output);
+		out(ObjArrToStrArr(args));
 	}
 
+	/*
+	 */
+	public static void err(String... args) {
+		String string = getTimestamp();
+		for( String str : args ) string += ":\t"+str;
+		System.err.println(string);
+	}
+	public static void err(Object... args) {
+		err(ObjArrToStrArr(args));
+	}
+
+	/*
+	 */
 	public static void debug(String... args) {
 		ArrayList<String> output = new ArrayList<String>();
 		output.add("DEBUG");
 		for( String str : args ) 
 			output.add(str);
 		out(output.toArray());
+	}
+	public static void debug(Object... args) {
+		debug(ObjArrToStrArr(args));
 	}
 }
